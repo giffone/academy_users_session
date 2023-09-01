0e3fa768ab2d641fc1ce206423fc8193f03730d1
fgalimzh <terre@academie.one>
2023-09-01 10:46:24 +0600

    add: models

---
+++ internal/domain/response.go
@@ -0,0 +1,6 @@
+package domain
+
+type Response struct {
+	Code   int    `json:"code"`
+	Status string `json:"status"`
+}
--- internal/domain/session.go
+++ internal/domain/session.go
@@ -1,1 +1,14 @@
 package domain
+
+type Session struct {
+	ID           string `json:"id" db:"id"`
+	ComputerName string `json:"comp_name" db:"comp_name"`
+	IPAddress    string `json:"ip_addr" db:"ip_addr"`
+	Login        string `json:"login" db:"login"`
+	Status       string `json:"status" db:"status"`
+	DateTime     int    `json:"date_time" db:"date_time"`
+}
+
+func (s *Session) validate() error {
+	return nil
+}
